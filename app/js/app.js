549437f941e59af0589328249742a784a73d6218
Ryan J. Peterson <ryan@mjg.in>
2012-09-17 10:45:33 -0400

    Update user login

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,6 @@
 // Declare app level module which depends on filters, and services
 angular.module('palettable', ['ngCookies', 'palettable.filters', 'palettable.services', 'palettable.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {template: 'partials/home.html', controller: AppCtrl});
     $routeProvider.when('/about', {template: 'partials/about.html', controller: AppCtrl});
-    $routeProvider.otherwise({redirectTo: '/'});
+    $routeProvider.otherwise({template: 'partials/home.html', controller: AppCtrl});
   }]);
